a9005d0d8c32be1448c19d1e306db49f7b0ee89e
Santosh Kumar <santoshkumar964887@gmail.com>
2020-09-27 18:38:26 +0530

    Updates

---
+++ precache-manifest.a0749fbcc8ea256c6df55b4b2258e8db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b63585c7a4c55c6997ac23ba4a3008c",
+    "url": "/React-Page-task/index.html"
+  },
+  {
+    "revision": "da12a577f15a1c0bea4d",
+    "url": "/React-Page-task/static/css/main.3d3813ec.chunk.css"
+  },
+  {
+    "revision": "1ae02c77fb2716609cb9",
+    "url": "/React-Page-task/static/js/2.ea55dd5d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Page-task/static/js/2.ea55dd5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da12a577f15a1c0bea4d",
+    "url": "/React-Page-task/static/js/main.58b6d1f5.chunk.js"
+  },
+  {
+    "revision": "558037888b35ce4de398",
+    "url": "/React-Page-task/static/js/runtime-main.181039a7.js"
+  },
+  {
+    "revision": "74421a7a2ab485ddba291404a7a0607e",
+    "url": "/React-Page-task/static/media/Capture.74421a7a.PNG"
+  }
+]);
